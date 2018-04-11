2e79d4ae9f87a792525a0de43f7e24bdefd9597e
Bert Overduin <boverduin@gmail.com>
2018-04-11 18:47:19 +0100

    Initial commit MVP

---
+++ hw_callbacks_enumeration_traveller_start/models/traveller.js
@@ -0,0 +1,34 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => journey.endLocation);
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+  return this.journeys.map((journey) => journey.transport);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => journey.transport === transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => journey.distance > minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
---
+++ hw_intro_to_context.md
@@ -0,0 +1,17 @@
+# Intro to Context
+
+## Brief
+
+Read the information in intro_to_context.md and answer the following questions:
+
+1. Can an arrow function expression's context change?
+
+No.
+
+2. How do we know what object `this` refers to in an arrow function expression body?
+
+3. Can a `function` expression's context change?
+
+Yes.
+
+4. How do we know what object `this` refers to in a `function` expression body?
